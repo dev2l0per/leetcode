0786096e8fdebfd4ab4fd9e26028f4a5ee6e73c2
양중효 <yangjunghyo@yangjunghyoui-MacBookPro-2.local>
2022-03-23 14:41:04 +0900

    Add 673 Solution

---
+++ Medium/673.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        dp = [0 for _ in range(len(nums))]
+        count = [1 for _ in range(len(nums))]
+        
+        for i in range(len(nums)):
+            dp[i] = 1
+            lengthOfSubsequence = 1
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    if dp[j] == dp[i]:
+                        dp[i] = dp[j] + 1
+                        count[i] = count[j]
+                    elif dp[j] + 1 == dp[i]:
+                        count[i] += count[j]
+        maxLength = max(dp)
+        return sum([count[i] for i in range(len(nums)) if dp[i] == maxLength])
+
+sol = Solution()
+print(sol.findNumberOfLIS([1, 3, 5, 4, 7]))
+print(sol.findNumberOfLIS([2, 2, 2, 2, 2]))
